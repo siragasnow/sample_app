bb9feced2e77fcae381dafdc68811b354127773d
siragasnow <siraga613@gmail.com>
2020-05-28 10:58:41 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key ='e3a5cc6ef0940dd848934d3e8248a603ff65521ffe576829afd92fa17c76852b30e8effd845a0c3b2ee270e2d99b3efedf5a427db23d9e81d4eb91567a1b0bdf'
